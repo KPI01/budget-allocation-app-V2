d440f9b00aca7293770010abe26ded18a0f7ba9c
KPI01 <jorgelurd11@gmail.com>
2023-08-07 18:27:32 +0200

    Updated App.js, index.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import Budget from "./components/Budget";
+import Remaining from "./components/Remaining";
+import { Provider } from "./context/AppContext";
 
 function App() {
   return (
-    <div className="container">
-        <h1 className="display-1 fw-bold">Company's Budget Allocation</h1>
-    </div>
+    <Provider>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h1 className="display-1 fw-bold">Company's Budget Allocation</h1>
+          </div>
+        </div>
+        <div className="row">
+          <Budget />
+          <Remaining />
+        </div>
+      </div>
+    </Provider>
   );
 }
 
